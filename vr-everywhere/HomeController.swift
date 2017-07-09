cd0c2f9c198fc2cfe283117e3b99e6bc69f58281
ey1031 <eyudeveloper@gmail.com>
2017-07-08 19:34:46 -0700

    adding audio recording

--- vr-everywhere/HomeController.swift
+++ vr-everywhere/HomeController.swift
@@ -12,7 +12,7 @@ import AVFoundation
 import Photos
 import Alamofire
 
-class HomeController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class HomeController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
     
     var session: AVCaptureSession!
     var input: AVCaptureDeviceInput!
@@ -20,6 +20,10 @@ class HomeController: UIViewController, UIImagePickerControllerDelegate, UINavig
     var previewLayer: AVCaptureVideoPreviewLayer!
     var base64String: String?
     var ngrok = "https://4adacd3b.ngrok.io/send?input=";
+    
+    var audioPlayer: AVAudioPlayer?
+    var audioRecorder: AVAudioRecorder?
+    
     
     @IBAction func trigger(_ sender: Any) {
         self.setupSession()
@@ -31,6 +35,66 @@ class HomeController: UIViewController, UIImagePickerControllerDelegate, UINavig
 //        }
     }
     
+    // record
+    @IBOutlet var noise: UIButton!
+    @IBAction func noise(_ sender: Any) {
+        print("noise")
+        if audioRecorder?.isRecording == false {
+            play.isEnabled = false
+            stop.isEnabled = true
+            audioRecorder?.record()
+        }
+    }
+    
+    @IBOutlet var stop: UIButton!
+    @IBAction func stop(_ sender: Any) {
+        print("stop")
+        stop.isEnabled = false
+        play.isEnabled = true
+        noise.isEnabled = true
+        
+        if audioRecorder?.isRecording == true {
+            audioRecorder?.stop()
+        } else {
+            audioPlayer?.stop()
+        }
+    }
+    
+    @IBOutlet var play: UIButton!
+    @IBAction func play(_ sender: Any) {
+        print("play")
+        if audioRecorder?.isRecording == false {
+            stop.isEnabled = true
+            noise.isEnabled = false
+            
+            do {
+                try audioPlayer = AVAudioPlayer(contentsOf:
+                    (audioRecorder?.url)!)
+                audioPlayer!.delegate = self
+                audioPlayer!.prepareToPlay()
+                audioPlayer!.play()
+            } catch let error as NSError {
+                print("audioPlayer error: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        noise.isEnabled = true
+        stop.isEnabled = false
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        print("Audio Play Decode Error")
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        print("Audio Record Encode Error")
+    }
+    
     func timerCalled(timer: Timer) {
         capturePhoto()
         print("HEYYYYYYYYYYYYYY")
@@ -39,6 +103,39 @@ class HomeController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        play.isEnabled = false
+        stop.isEnabled = false
+        
+        let fileMgr = FileManager.default
+        
+        let dirPaths = fileMgr.urls(for: .documentDirectory,
+                                    in: .userDomainMask)
+        
+        let soundFileURL = dirPaths[0].appendingPathComponent("sound.caf")
+        
+        let recordSettings =
+            [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+             AVEncoderBitRateKey: 16,
+             AVNumberOfChannelsKey: 2,
+             AVSampleRateKey: 44100.0] as [String : Any]
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(
+                AVAudioSessionCategoryPlayAndRecord)
+        } catch let error as NSError {
+            print("audioSession error: \(error.localizedDescription)")
+        }
+        
+        do {
+            try audioRecorder = AVAudioRecorder(url: soundFileURL,
+                                                settings: recordSettings as [String : AnyObject])
+            audioRecorder?.prepareToRecord()
+        } catch let error as NSError {
+            print("audioSession error: \(error.localizedDescription)")
+        }
     }
     
     override func didReceiveMemoryWarning() {
